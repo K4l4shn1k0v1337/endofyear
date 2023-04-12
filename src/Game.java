2d2c79eccd7b5671364b5ccfb6a472bfaa3904ea
s2165094 <s2165094@L008MXL0042ZG8.student.HISD.ORG>
2023-04-12 11:23:57 -0500

    updated game

--- src/Game.java
+++ src/Game.java
@@ -1,82 +1,70 @@
+import javax.swing.*;
 import javax.swing.ImageIcon;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
 
-public class Tank {
-    private int x, y, w, h,dx;
-    private ImageIcon img;
+
+public class Game extends JPanel implements Runnable {
+	private BufferedImage back;
+  private ImageIcon background, backeffect;
+  private Tank player1, player2;
+
+	 private boolean playMusic;
+	 private Sound s;
+  //tt
+	
+	public Game() {
+    background = new ImageIcon ("background.jpg");
+    backeffect = new ImageIcon ("BOTback.gif");
+		back=null;
+    player1 = new Tank(250,300,100,100, new ImageIcon("bluetank.png"));
+	player2 = new Tank(1450,300,100,100, new ImageIcon("greentank.png"));
+   Sound s =new Sound();
+    s.playmusic("battle-of-the-dragons-8037.wav");
+                        		
+
+
 
     
-	public Tank()
-	{
-		x=700;
-		y=700;
-		dx=0;
-		w=100;
-		h=100;
-		img = new ImageIcon("bluetank.png");
-	
-	}
-    
-	public Tank(int xV, int yV, int width, int height, String s)
-	{
-		x=xV;
-		y=yV;
-		dx=0;
-		w=width;
-		h=height;
-		img = new ImageIcon (s);
-	}
-	
-	
-	
-	
-	public void setX(int xV) {
-	x+=xV;
-	}
-	
-	public void setY(int yV) {
-		y += yV;
-	}
-	
-	public int getX()
-	{
-		return x;
-	}
-	
-	public int getY()
-	{
-		return y;
-	}
-	
-	
-	public void setW(int width) {
-		w+=width;
-	}
-	
-	public void setH(int height) {
-		h+=height;
-	}
-	
-	public int getW() {
-		return w;
-	}
-	public int getH() {
-		return h;
-	}
-
-	
-	public void move()
-	{
-		x+=dx;
 		
 	}
 	
-	public void setDx(int dxFromGame)
+	public void run() {
+		try {
+			while(true) {
+				Thread.currentThread().sleep(5);
+				repaint();
+			}
+		}
+		catch(Exception e) {}                               
+	}
+
+
+
+  
+	
+	public void paint (Graphics g)
 	{
-		dx= dxFromGame;
+		Graphics2D twoDgraph = (Graphics2D)g;
+
+		if (back==null) {
+			back =(BufferedImage) (createImage(getWidth(), getHeight()));
+				}
+
+		Graphics g2d = back.createGraphics();
+		g2d.clearRect(0, 0, getSize().width, getSize().height); 
+		g2d.drawImage(background.getImage(), 0, 0, getWidth(), getHeight(), this); 
+
+	
+
+		g2d.drawImage(player1.getImg().getImage(), player1.getX(), player1.getY(), player1.getW(), player1.getH(), this);
+		g2d.drawImage(player2.getImg().getImage(), player2.getX(), player2.getY(), player2.getW(), player2.getH(), this);
+
+		
+		
+		twoDgraph.drawImage(back, 0, 0, null);
 	}
 	
-	public ImageIcon getImg() {
-		return img;
-	}	
-	
 }
+
