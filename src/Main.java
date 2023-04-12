cf84ec7c1bbc9899b46f570d1cedbb95ad517667
s2181741 <s2181741@L008MXL0042ZFZ.student.HISD.ORG>
2023-04-12 10:53:48 -0500

    in process

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
-
-public class Main {
-	
-	//hELLO
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class Main extends JFrame{
+private static final int WIDTH =1920;
+private static final int HEIGHT=1080;
+public Main () {
+super("Demo Graphics");
+setSize(WIDTH, HEIGHT);
+Game play = new Game();
+((Component) play).setFocusable(true);
+setBackground(Color.MAGENTA);
+getContentPane().add(play);
+setVisible(true);
+setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 }
-
-
+public static void main(String[] args) {
+Main run = new Main();
+}
+}
