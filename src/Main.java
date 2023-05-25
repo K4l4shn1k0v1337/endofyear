62eb690d5fd1ade13619154f1f5de1822a499e0b
s2181741 <s2181741@L008MXL0042ZFZ.student.HISD.ORG>
2023-05-25 09:53:09 -0500

    submit

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,34 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Main extends JFrame {
-    private static final int WIDTH = 1920;
-    private static final int HEIGHT = 1080;
+public class Main extends JFrame{
+	private static final int WIDTH =1800;
+	private static final int HEIGHT=1600;
+	
+	public Main () {
+		super("Space Invaders");
+		setSize(WIDTH, HEIGHT);
+		Game play = new Game();
+		((Component) play).setFocusable(true);
+		
+		Color RoyalBlue = new Color(191,213,232);
+		
+		
+		setBackground(RoyalBlue);
+		
+		
+		getContentPane().add(play);
+		
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
 
-    public Main() {
-        super("BOF");
-        setUndecorated(true); // Remove window decorations (title bar, borders)
-        setExtendedState(JFrame.MAXIMIZED_BOTH); // Maximize the window to full-screen
+	public static void main(String[] args) {
+		Main run = new Main();
+		
 
-        GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice device = env.getDefaultScreenDevice();
-
-        if (device.isFullScreenSupported()) {
-            device.setFullScreenWindow(this); // Set the window to full-screen
-        } else {
-            System.err.println("Full-screen mode not supported.");
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            setSize(WIDTH, HEIGHT);
-            setLocationRelativeTo(null);
-        }
-
-        Game play = new Game();
-        ((Component) play).setFocusable(true);
-        getContentPane().add(play);
-
-        setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            Main run = new Main();
-        });
-    }
+	}
 }
